f512f55c0f2a3d5a97d62b5967b66d08fa94bb40
pkinnaird <kinnaird.peter@gmail.com>
2016-01-30 17:46:47 +0000

    Got the rendering working well, looks okay now

--- share/spice/jisho/jisho.js
+++ share/spice/jisho/jisho.js
@@ -6,16 +6,37 @@
         var japaneseReading = item.japanese[0].reading
         var senses = item.senses;
         
+        var senseBuckets = {};
+        var partsOfSpeech;
+        
+        //So we have to do a few things - loop over all the things and put them into their correct bucket of speech
+        //and then remove the wikipedia definitions.
         for (var j = 0; j < senses.length; j++){
-            for (var i = 0; i < senses[j].parts_of_speech.length; i++){
-                if (senses[j].parts_of_speech[i] === 'Wikipedia definition'){
-                    delete senses[j];
-                    break;
-                }
-            }     
+            if (senses[j].parts_of_speech.join("").trim() !== ""){
+                partsOfSpeech = senses[j].parts_of_speech.join(", ");
+            }
+            var limit = (senses[j].parts_of_speech.length > 6) ? 6 : senses[j].parts_of_speech.length;
+            for (var i = 0; i < limit; i++){
+                if (senses[j].parts_of_speech[i] !== 'Wikipedia definition'){
+                    if (senseBuckets[partsOfSpeech] === undefined){
+                        senseBuckets[partsOfSpeech] = []
+                    }
+                    senseBuckets[partsOfSpeech] = senseBuckets[partsOfSpeech].concat(senses[j].english_definitions);
+                     
+                } 
+            }
+            senses[j].bucket = senseBuckets[partsOfSpeech];
         }
-
-        return {"japanese": japaneseWord, "reading": japaneseReading, "senses": senses};
+        
+        var out = [];
+        var senseKeys = Object.keys(senseBuckets);
+        senseKeys.forEach(function(key){
+            var bucket = senseBuckets[key];
+            out.push({key: key, value: bucket});
+        })
+        console.log({"japanese": japaneseWord, "reading": japaneseReading, "senses": out});
+        return {"japanese": japaneseWord, "reading": japaneseReading, "senses": out};
+        
     }
     
     env.ddg_spice_jisho = function(api_result){
